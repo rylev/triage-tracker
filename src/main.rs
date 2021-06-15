99600490f70a4757435c6a610cc5f74b9c26c564
Ryan Levick <me@ryanlevick.com>
2021-06-15 12:03:22 +0200

    Handle range

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,43 @@ async fn main() -> Result<()> {
                 .value_name("DATE")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("start")
+                .short("s")
+                .long("start")
+                .value_name("DATE")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("end")
+                .short("e")
+                .long("end")
+                .value_name("DATE")
+                .takes_value(true),
+        )
         .get_matches();
-    let date = matches
-        .value_of("date")
-        .map(|d| d.parse::<chrono::NaiveDate>().unwrap())
-        .unwrap_or_else(|| chrono::Utc::today().pred().naive_utc());
+    match (
+        matches.value_of("date"),
+        matches.value_of("start"),
+        matches.value_of("end"),
+    ) {
+        (Some(d), None, None) => {
+            let date = d.parse::<chrono::NaiveDate>().unwrap();
+            handle_date(date).await?;
+        }
+        (None, Some(s), Some(e)) => {
+            let start = s.parse::<chrono::NaiveDate>().unwrap();
+            let end = e.parse::<chrono::NaiveDate>().unwrap();
+            handle_range(start, end).await?;
+        }
+        _ => {
+            eprintln!("INVALID ARGS! TODO: Print help");
+        }
+    }
+    Ok(())
+}
+
+async fn handle_date(date: chrono::NaiveDate) -> Result<()> {
     let items = Issues::for_date(date).await?;
 
     println!("On {}", date.format("%Y-%m-%d"));
@@ -30,6 +62,104 @@ async fn main() -> Result<()> {
     for i in items.closed() {
         println!("  {}", i);
     }
+    Ok(())
+}
+
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::style::{Color, Modifier, Style};
+use tui::text::Span;
+use tui::widgets::{BarChart, Block, Borders};
+use tui::Terminal;
+
+async fn handle_range(start: chrono::NaiveDate, end: chrono::NaiveDate) -> Result<()> {
+    if end >= start {
+        return Err("--start must be more recent than --end".into());
+    }
+    let mut issues = Vec::new();
+    let mut date = start;
+    loop {
+        issues.push((date, Issues::for_date(date).await?));
+        date = date.pred();
+        if date == end.pred() {
+            break;
+        }
+    }
+    let data = std::sync::Arc::new(
+        issues
+            .into_iter()
+            .map(|(date, issues)| {
+                (
+                    date.format("%Y-%m-%d").to_string(),
+                    issues.opened().count() as u64,
+                )
+            })
+            .collect::<Vec<_>>(),
+    );
+
+    // Terminal initialization
+    let stdout = std::io::stdout().into_raw_mode()?;
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    enum Event {
+        Key(termion::event::Key),
+        Tick,
+    }
+    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
+    let stdin = std::io::stdin();
+    use termion::input::TermRead;
+    let tx_clone = tx.clone();
+    tokio::spawn(async move {
+        for evt in stdin.keys() {
+            if let Ok(key) = evt {
+                if let Err(_) = tx_clone.send(Event::Key(key)).await {
+                    return;
+                }
+            }
+        }
+    });
+    tokio::spawn(async move {
+        loop {
+            if let Err(_) = tx.send(Event::Tick).await {
+                break;
+            }
+            tokio::time::sleep(std::time::Duration::from_millis(500)).await;
+        }
+    });
+    loop {
+        match rx.recv().await {
+            Some(Event::Key(termion::event::Key::Char('q'))) | None => {
+                break;
+            }
+            _ => {}
+        }
+        let data = data.clone();
+        terminal.draw(move |f| {
+            let size = f.size();
+            let d = data
+                .iter()
+                .map(|(s, n)| (s.as_str(), *n))
+                .collect::<Vec<(&str, u64)>>();
+            let chart = BarChart::default()
+                .block(
+                    Block::default()
+                        .title(Span::styled(
+                            "Issues Opened",
+                            Style::default()
+                                .fg(Color::Cyan)
+                                .add_modifier(Modifier::BOLD),
+                        ))
+                        .borders(Borders::ALL),
+                )
+                .bar_width(10)
+                .bar_style(Style::default().fg(Color::LightBlue))
+                .data(d.as_slice());
+            f.render_widget(chart, size);
+        })?;
+    }
+    rx.close();
     Ok(())
 }
 
@@ -220,6 +350,13 @@ impl Issues {
             .iter()
             .filter(|i| matches!(i.state_change(), StateChange::Closed))
             .map(|i| i.issue())
+    }
+
+    #[allow(dead_code)]
+    fn diff(&self) -> isize {
+        let opened = self.opened().count() as isize;
+        let closed = self.closed().count() as isize;
+        opened - closed
     }
 }
 
